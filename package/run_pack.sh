87afa3aadcefa9892e2fd3159d331d9112ba4145
liziru <youmakemewhole@163.com>
2021-10-29 20:54:58 +0800

    v1.1

--- package/run_pack.sh
+++ package/run_pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "INFO: start packing."
-pyinstaller -F ../openi_player_main.py  --hidden-import='PIL._tkinter_finder'
-echo "INFO: Dne."
+pyinstaller -F ../openi_player_main.py  --hidden-import='PIL._tkinter_finder' --add-data '../download/images/:download/images'
+echo "INFO: Done."
